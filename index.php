89e5a267ef9f00f59a5d30b22fa4a9c05cce0b50
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-14 09:43:42 +0200

    refactor index.php, php include links_header

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MaxRider</title>
   <link rel="stylesheet" href="css/index.css">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://unpkg.com/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
   <?php include "links_header.php"; ?>
 </head>
 <body>
