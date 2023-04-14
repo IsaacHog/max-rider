65ff0cf4bbac09583cf4cbde9d225ba0b718dc28
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-14 13:50:52 +0200

    progress commit

--- solicitar_pedido.php
+++ solicitar_pedido.php
@@ -56,16 +56,33 @@
                         <h5 class="mb-0 fw-bold">Entrega</h5>
                     </div>
                 </div>
-                <div class="row m-2 align-items-center">
-                    <button class="btn btn-light dropdown-toggle border border-1 rounded-5">
-                        Seleccionar punto de recogida
-                    </button>
+
+                <div class="row m-2 align-items-center bg-white">
+                    <div class="input-group border rounded-5">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-transparent border-0">
+                                <i class="bi bi-search text-primary fs-5"></i>
+                            </span>
+                        </div>
+                        <input class="form-control border-0" placeholder="Buscar una dirección">
+                    </div>
                 </div>
+
                 <div class="row">
                     <div class="row align-items-center">
-                        <small class="form-text text-muted text-center fs-6">Crear un
-                            <a href="#">nuevo contacto</a>
-                        </small>
+                        <small class="form-text text-black fs-6">Persona de contacto</small>
+                    </div>
+                    <div class="col-6 mb-3">
+                        <input class="form-control border-0 rounded-5" placeholder="Nombre">
+                    </div>
+                    <div class="col-6">
+                    <input class="form-control border-0 rounded-5" placeholder="Apellidos">
+                    </div>
+                    <div class="col-6">
+                        <input class="form-control border-0 rounded-5" placeholder="Compañía">
+                    </div>
+                    <div class="col-6">
+                    <input class="form-control border-0 rounded-5" placeholder="Teléfono">
                     </div>
                 </div>
             </div>
