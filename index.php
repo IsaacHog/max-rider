4164727314cf337384c1ed26ee5b85dd82bd9ac5
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-14 09:43:07 +0200

    refactor index.php, php include links_header

--- index.php
+++ index.php
@@ -9,12 +9,6 @@
   <?php include "links_header.php"; ?>
 </head>
 <body>
-
   <?php include("navbar.php"); ?>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
-
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MaxRider - Login</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
