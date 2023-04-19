57dff1715e3804eb4cbb88c3195932bd420d04ba
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-19 10:18:12 +0200

    add test open-street-maps

---
+++ open-street-maps/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MaxRider</title>
+    <?php include "../links_header.php"; ?>
+  </head>
+  <body class="bg-light">
+    <?php include("../navbar.php"); ?>
+  </body>
+</html>
