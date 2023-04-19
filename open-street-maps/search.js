65f70a592895e0f8d4cd7d551bc1eb539e19ad02
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-19 14:51:13 +0200

    add lookup API

--- open-street-maps/search.js
+++ open-street-maps/search.js
@@ -4,21 +4,32 @@ function search() {
     var query = encodeURIComponent(document.getElementById('query').value);
 
     // Reduce API stress
-    if (query.length < 3) { return; }
+    if (query.length < 3) { 
+        return; 
+    }
+
     if (timeoutId) {
         clearTimeout(timeoutId);
     }
 
     timeoutId = setTimeout(function() {
-        var url = "https://nominatim.openstreetmap.org/search?q=" + query + "&format=jsonv2&addressdetails=1";
-        fetch(url, { headers: { 'User-Agent': 'My Application' } })
+        var searchUrl = "https://nominatim.openstreetmap.org/search?q=" + query + "&format=jsonv2&addressdetails=1&limit=1";
+
+        fetch(searchUrl, { headers: { 'User-Agent': 'My Application' } })
             .then(response => response.json())
             .then(data => {
-                var results = '';
-                data.forEach(result => {
-                    results += result.display_name + '<br>';
-                });
-                document.getElementById('search-results').innerHTML = results;
+                var searchResult = data[0];
+
+                var lookupUrl = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + searchResult.lat + "&lon=" + searchResult.lon + "&zoom=18&addressdetails=1";
+
+                fetch(lookupUrl, { headers: { 'User-Agent': 'My Application' } })
+                    .then(response => response.json())
+                    .then(data => {
+                        var address = data.display_name;
+
+                        document.getElementById('search-results').innerHTML = address;
+                    })
+                    .catch(error => console.error(error));
             })
             .catch(error => console.error(error));
     }, 500); // wait 500ms before making the API call
