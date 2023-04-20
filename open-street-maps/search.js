196d235e32cbc2679d952e85be3b07ffc7897c7e
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-20 12:10:37 +0200

    refactor search.js

--- open-street-maps/search.js
+++ open-street-maps/search.js
@@ -1,59 +1,44 @@
 
 import { debounce } from './debounce.js';
 
-function search() {
-    console.log("searching...")
-    var query = encodeURIComponent(document.getElementById('query').value);
+async function search() {
+    console.log("searching...");
+    const query = encodeURIComponent(document.getElementById('query').value);
     if (query.length < 3 || query.length === 0) {
-        $("#search-results").html(
-            `<div class="card">
+        $("#search-results").html(`
+            <div class="card">
                 <div class="card-header">Address:</div>
                 <div class="card-body">
                     <p>No results</p>
                 </div>
-            </div>`);
+            </div>
+        `);
         return;
     }
 
-    var searchUrl = `https://nominatim.openstreetmap.org/search?format=geojson&limit=5&street=${query}`;
-    fetch(searchUrl, { headers: { 'User-Agent': 'My Application' } })
-        .then(response => response.json())
-        .then(data => {
-            var addressHtml = '';
-            data.features.forEach((element) => {
-                addressHtml += `
-                    <a href="#" class="list-group-item list-group-item-action search-result" data-lat='${element.geometry.coordinates[1]}' data-long='${element.geometry.coordinates[0]}'>
-                        ${element.properties.display_name}
-                    </a>
-                `;
-            });
-            if (addressHtml === '') {
-                $("#search-results").html(
-                    `<div class="card">
-                        <div class="card-header">Address:</div>
-                        <div class="card-body">
-                            <p>No results</p>
-                        </div>
-                    </div>`
-                );
-            } else {
-                $("#search-results").html(
-                    `<div class="card">
-                        <div class="card-header">Address:</div>
-                        <div class="list-group list-group-flush">${addressHtml}</div>
-                    </div>`
-                );
-            }
+    const searchUrl = `https://nominatim.openstreetmap.org/search?format=geojson&limit=5&street=${query}`;
+    try {
+        const response = await fetch(searchUrl, { headers: { 'User-Agent': 'My Application' } });
+        const data = await response.json();
+        const addressHtml = data.features.map((element) => `
+            <a href="#" class="list-group-item list-group-item-action search-result" data-lat='${element.geometry.coordinates[1]}' data-long='${element.geometry.coordinates[0]}'>
+                ${element.properties.display_name}
+            </a> `).join("");
 
-            attachClickHandler();
-        })
-        .catch(error => console.error(error));
+        $("#search-results").html(`
+            <div class="card">
+                <div class="card-header">Address:</div>
+                <div class="list-group list-group-flush">${addressHtml}</div>
+            </div> `);
+
+        attachClickHandler();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const debouncedSearch = debounce(search, 500);
 document.getElementById('query').addEventListener('input', debouncedSearch);
-
-
 
 let previousMarker = null;
 function attachClickHandler() {
@@ -62,7 +47,6 @@ function attachClickHandler() {
         $(searchResults[i]).one('click', function () {
             var lat = this.dataset.lat;
             var long = this.dataset.long;
-
             $("#start").val($(this).text());
 
             if (previousMarker) {
@@ -85,9 +69,8 @@ function attachClickHandler() {
                 map.panTo(new L.latLng(position.lat, position.lng));
             });
 
-            var zoom = 13;
+            var zoom = 5; // 13 default
             map.setView([lat, long], zoom);
-
             previousMarker = startMarker;
 
             $("#search-results").html('');
@@ -96,18 +79,10 @@ function attachClickHandler() {
 }
 
 
-function findAdress(lat, long, marker) {
-    map.eachLayer(function (layer) {
-        if (layer instanceof L.Polyline) {
-            map.removeLayer(layer);
-        }
-    });
-    const api = 'https://nominatim.openstreetmap.org/reverse?lat=' + lat + '&lon=' + long + '&format=json';
-    $.ajax({
-        url: api,
-        method: 'GET',
-    })
-        .done(function (data) {
-            $("#" + marker).val(data.display_name)
-        })
+async function findAdress(lat, long, marker) {
+    const api = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`;
+    const response = await fetch(api);
+    const data = await response.json();
+    $(`#${marker}`).val(data.display_name);
 }
+
