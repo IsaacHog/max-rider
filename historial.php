8964d2d47b6179ff18d4cc926faa374f67999c7e
sakdasaleephan <sakdasaleephan2@gmail.com>
2023-04-25 16:18:33 +0200

    Klar med activos typ, moving over to historial.php

---
+++ historial.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MaxRider</title>
+    <?php include "links_header.php"; ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script type="text/javascript" src="https://rawgit.com/jieter/Leaflet.encoded/master/Polyline.encoded.js"></script>
+</head>
+
+<body class="bg-light">
+    <?php include("navbar.php"); ?>
+</body>
+
+</html>
