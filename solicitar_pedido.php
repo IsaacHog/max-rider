17856a87846bf742280c45ed7f44fec351e955a2
sakdasaleephan <sakdasaleephan2@gmail.com>
2023-04-25 11:33:04 +0200

    commit pop up

--- solicitar_pedido.php
+++ solicitar_pedido.php
@@ -173,7 +173,26 @@
                         </div>
                     </div>
                     <div class="col-2 col-xxl-1 d-flex justify-content-end">
-                        <button class="btn btn-primary">Solicitar</button>
+                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Solicitar</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Confirmar solicitud</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ¿Está seguro que desea solicitar este producto?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="button" class="btn btn-primary">Solicitar</button>
+                        </div>
                     </div>
                 </div>
             </div>
