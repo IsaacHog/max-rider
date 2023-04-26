ceea8c96041e84fc1efbbfad4a2c098ef2a2a043
sakdasaleephan <sakdasaleephan2@gmail.com>
2023-04-26 16:17:19 +0200

    Done with styling in soporte.php

--- soporte.php
+++ soporte.php
@@ -31,10 +31,79 @@
             </div>
 
             <!--Border with content-->
-            <div class="col-10 border border-2 rounded-4 p-5">
+            <div class="col-10 border border-2 rounded-4 p-5 mb-3">
+                <!--Header-->
                 <div class="row mb-3">
                     <div class="col-6">
-                        <h4 class="fw-bold">soporte</h4>
+                        <h4 class="fw-bold">Preguntas frecuentes</h4>
+                    </div>
+                </div>
+                <!--Lorem ipsum texts-->
+                <div class="row mb-3">
+                    <div class="col-12 border border-1 rounded-4 d-flex">
+                        <div class="col-6 mt-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-6 mt-3 d-flex justify-content-end">
+                            <i class="bi bi-arrow-down fs-5 text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 border border-1 rounded-4 d-flex">
+                        <div class="col-6 mt-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-6 mt-3 d-flex justify-content-end">
+                            <i class="bi bi-arrow-down fs-5 text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 border border-1 rounded-4 d-flex">
+                        <div class="col-6 mt-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-6 mt-3 d-flex justify-content-end">
+                            <i class="bi bi-arrow-down fs-5 text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 border border-1 rounded-4 d-flex">
+                        <div class="col-6 mt-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-6 mt-3 d-flex justify-content-end">
+                            <i class="bi bi-arrow-down fs-5 text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 border border-1 rounded-4 d-flex">
+                        <div class="col-6 mt-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-6 mt-3 d-flex justify-content-end">
+                            <i class="bi bi-arrow-down fs-5 text-primary"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Second border with content-->
+        <div class="row">
+            <div class="col-2">
+
+            </div>
+            <div class="col-10 border border-2 rounded-4 p-5">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h4 class="fw-bold mb-0">Soporte</h4>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end">
+                        <i class="bi bi-envelope fs-2 text-primary"></i>
                     </div>
                 </div>
             </div>
