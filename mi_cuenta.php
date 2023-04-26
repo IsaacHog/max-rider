132c4def677f53d0409ce92e471fb735de0c7173
sakdasaleephan <sakdasaleephan2@gmail.com>
2023-04-26 14:15:03 +0200

    Mi_cuenta.php basically klar. fortsätter till nästa

--- mi_cuenta.php
+++ mi_cuenta.php
@@ -14,18 +14,74 @@
 <body class="bg-light">
     <?php include("navbar.php"); ?>
 
-    <div class="container">
-        <!--Little side navBar-->
-        <div class="col-2">
-            <div class="row">
-                <button type="button" class="text-primary"> Mi Cuenta</button>
+    <div class="container mt-5">
+        <div class="row">
+            <!--Little side navBar-->
+            <div class="col-2">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid flex-column">
+                        <div class="navbar-nav flex-column">
+                            <a class="nav-link text-primary border border-2 border-primary rounded-pill fw-bold" href="#">Mi cuenta</a>
+                            <a class="nav-link fw-bold" href="#">Facturación</a>
+                            <a class="nav-link fw-bold" href="#">Mis contactos</a>
+                            <a class="nav-link fw-bold" href="#">Soporte</a>
+                        </div>
+                    </div>
+                </nav>
             </div>
-        </div>
 
-        <!--Border with content-->
-        <div class="col-10">
-            <div>
-
+            <!--Border with content-->
+            <div class="col-10 border border-2 rounded-4 p-5">
+                <div class="row mb-3">
+                    <h4 class="fw-bold">Mi cuenta</h4>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-6">
+                        <input class="form-control border-1 rounded-4" placeholder="Empresa o particular">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <h5 class="fw-bold">Información empresa</h5>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-6">
+                        <input class="form-control border-1 rounded-4" placeholder="Nombre de la empresa">
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control border-1 rounded-4" placeholder="Sector">
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control border-1 rounded-4" placeholder="Envíos mensuales">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <h5 class="fw-bold">Información general</h5>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-6">
+                        <input class="form-control border-1 rounded-4" placeholder="Nombre">
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control border-1 rounded-4" placeholder="Ciudad">
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control border-1 rounded-4" placeholder="Teléfono">
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-6">
+                        <input class="form-control border-1 rounded-4" placeholder="Email">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 d-flex">
+                        <p>¿Quieres cambiar tu contraseña?</p>
+                        <a class="text-decoration-none" href="#">&nbsp Restrablecer contraseña</a>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end">
+                        <a class="btn btn-primary rounded-4" href="">Guardar cambios</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
