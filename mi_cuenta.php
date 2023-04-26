7359e8ff3413eb34a99bfe938c726c4b1810593f
sakdasaleephan <sakdasaleephan2@gmail.com>
2023-04-26 11:38:05 +0200

    Created and added fundamentals in mi_cuenta.php

---
+++ mi_cuenta.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MaxRider</title>
+    <?php include "links_header.php"; ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script type="text/javascript" src="https://rawgit.com/jieter/Leaflet.encoded/master/Polyline.encoded.js"></script>
+</head>
+
+<body class="bg-light">
+    <?php include("navbar.php"); ?>
+
+    <div class="container">
+        <!--Little side navBar-->
+        <div class="col-2">
+            <div class="row">
+                <button type="button" class="text-primary"> Mi Cuenta</button>
+            </div>
+        </div>
+
+        <!--Border with content-->
+        <div class="col-10">
+            <div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
