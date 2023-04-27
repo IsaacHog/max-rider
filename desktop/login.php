c6bb3ddf5aa615d763c5527c0cbf3640fb923b18
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-27 10:11:46 +0200

    remake login.php, fix css files in desktop

--- desktop/login.php
+++ desktop/login.php
@@ -1,47 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MaxRider - Login</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/login.css">
-  <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
-
+  <?php include "links_header.php"; ?>
 </head>
-<body>
 
-  <div class="container-fluid h-100">
-    <div class="row h-100">
+<body class="overflow-hidden">
+  <div class="container-fluid">
+    <div class="row">
 
-      <div class="col-md-6 left-side">
-
-        <h3 class="mb-4 text-md-center" id="company-title">MaxRider</h3>
-
-        <div class="form-container">
-          <h5 class="mb-4" style="font-size: 28px; font-weight: 700;">Iniciar sesión en MaxRider</h5>
-
-          <div class="form-group">
-            <input type="email" class="form-control rounded-pill" id="email" placeholder="Email">
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control rounded-pill" id="password" placeholder="Contraseña">
-            <small class="form-text text-muted text-right"><a href="#">¿Olvidaste tu contraseña?</a></small>
-          </div>
-          <button type="submit" class="btn btn-login btn-block mb-3 rounded-pill">Iniciar sesión</button>
-
-          <small class="form-text text-muted text-center" style="font-size: 16px; ">¿Aún no tienes cuenta? <a href="#">Regístrate</a></small>
+      <div class="col-md-6 d-flex align-items-center justify-content-center position-relative">
+        <div class="logo-container position-absolute top-0 start-0 pt-3 ps-2">
+          <a class="d-flex align-items-center text-decoration-none" href="../index.php">
+            <span class="text-primary fs-1" style="font-family: 'Visby CF', sans-serif; font-weight: normal;">Max</span>
+            <span class="text-primary fs-1" style="font-family: 'Visby CF', sans-serif; font-weight: bold;">Rider</span>
+          </a>
         </div>
 
+        <div class="container-fluid pt-3 d-flex flex-column align-items-center">
+          <div class="row">
+            <h5 class="fw-bold fs-2">Iniciar sesión en MaxRider</h5>
+          </div>
+
+          <div class="form-group pb-3 w-75">
+            <input type="email" class="form-control rounded-pill form-control-lg" id="email" placeholder="Email">
+          </div>
+          <div class="form-group w-75">
+            <input type="password" class="form-control rounded-pill form-control-lg" id="password"
+              placeholder="Contraseña">
+          </div>
+          <div class="row w-75 pt-2">
+            <p class="form-text text-end"><a href="#" class="text-decoration-none fs-6">¿Olvidaste tu contraseña?</a>
+            </p>
+          </div>
+
+          <div class="row w-75">
+            <button class="btn btn-primary mb-3 p-2 rounded-4 ps-5 pe-5 fs-5">Iniciar sesión</button>
+          </div>
+
+          <p class="form-text text-black fs-6">
+            ¿Aún no tienes cuenta?<a href="#" class="text-decoration-none fs-6">
+              Regístrate</a></small>
+          </p>
+        </div>
       </div>
 
-      <div class="col-md-6 right-side"></div>
-
+      <div class="col-md-6" style="  height: 100vh; background-image: url('../assets/login-image.png'); background-size: contain; background-repeat: no-repeat;
+        background-color: #0047FF; background-position: center;"></div>
     </div>
   </div>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 </body>
+
 </html>
