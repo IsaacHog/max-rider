c7c70d07ab60f05164852afdf281d05168c24672
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-04-27 14:39:57 +0200

    remove unsued scripts from mobile files

--- mobile/soporte.php
+++ mobile/soporte.php
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MaxRider</title>
     <?php include "links_header.php"; ?>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script type="text/javascript" src="https://rawgit.com/jieter/Leaflet.encoded/master/Polyline.encoded.js"></script>
 </head>
 
 <body class="bg-light">
